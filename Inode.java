e941bb69e20c5569c870d851d29cefc3da190f35
Renee <rnelso@uw.edu>
2022-11-28 16:11:50 -0800

    no fork no fork no fork

--- Inode.java
+++ Inode.java
@@ -1,5 +1,5 @@
 public class Inode {
-	//test edit
+	//test edit 3
 	public final static int iNodeSize = 32;  // fixed to 32 bytes
     public final static int directSize = 11; // # direct pointers
 

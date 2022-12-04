c4bc2963c04d32117c7dd4731fb0c9cd1b90e0ee
Zach Clow <zachclow@uw.edu>
2022-12-04 15:33:46 -0800

    Added SysLib.java for code completion's sake, Directory should be finished, but not tested Set up some stuff for FileTable
    I left some TODOs in that have been finished already,
    they're just there to help locate the recently
    modified code again.

--- FileTable.java
+++ FileTable.java
@@ -1,40 +1,56 @@
+import java.util.Vector;
 
 public class FileTable {
 // File Structure Table
 
     private Vector<FileTableEntry> table;// the entity of File Structure Table
     private Directory dir;         // the root directory
-    
-    public FileTable ( Directory directory ) {// a default constructor
-	table = new Vector<FileTableEntry>( );// instantiate a file table
-	dir = directory;                      // instantiate the root directory
+
+    public FileTable(Directory directory) {// a default constructor
+        table = new Vector<FileTableEntry>();// instantiate a file table
+        dir = directory;                      // instantiate the root directory
     }
 
-	// you implement
-	public synchronized FileTableEntry falloc( String fname, String mode ) {
-	}
-
-    public synchronized boolean ffree( FileTableEntry e ) {
-	// receive a file table entry
-	// free the file table entry corresponding to this index
-	if ( table.removeElement( e ) == true ) { // find this file table entry
-	    e.inode.count--;       // this entry no longer points to this inode
-	    switch( e.inode.flag ) {
-	    case 1: e.inode.flag = 0; break;
-	    case 2: e.inode.flag = 0; break;
-	    case 4: e.inode.flag = 3; break;
-	    case 5: e.inode.flag = 3; break;
-	    }
-	    e.inode.toDisk( e.iNumber );     // reflect this inode to disk
-	    e = null;                        // this file table entry is erased.
-	    notify( );
-	    return true;
-	} else
-	    return false;
+    //TODO: you implement
+    public synchronized FileTableEntry falloc(String fname, String mode) {
+        // allocate a new file (structure) table entry for this file name
+        FileTableEntry target = new FileTableEntry();
+        table.add(target);
+        // allocate/retrieve and register the corresponding inode using dir
+        // increment this inode's count
+        // immediately write back this inode to the disk
+        // return a reference to this file (structure) table entry
+        return target;
     }
 
-    public synchronized boolean fempty( ) {
-	return table.isEmpty( );             // return if table is empty
+    public synchronized boolean ffree(FileTableEntry e) {
+        // receive a file table entry
+        // free the file table entry corresponding to this index
+        if (table.removeElement(e) == true) { // find this file table entry
+            e.inode.count--;       // this entry no longer points to this inode
+            switch (e.inode.flag) {
+                case 1:
+                    e.inode.flag = 0;
+                    break;
+                case 2:
+                    e.inode.flag = 0;
+                    break;
+                case 4:
+                    e.inode.flag = 3;
+                    break;
+                case 5:
+                    e.inode.flag = 3;
+                    break;
+            }
+            e.inode.toDisk(e.iNumber);     // reflect this inode to disk
+            e = null;                        // this file table entry is erased.
+            notify();
+            return true;
+        } else
+            return false;
+    }
+
+    public synchronized boolean fempty() {
+        return table.isEmpty();             // return if table is empty
     }                                        // called before a format
-}
 }

dbd6a565b96d4ad7d263b0b9f04a65cd80211837
Renee <rnelso@uw.edu>
2022-12-05 16:13:38 -0800

    Inode, completed toDisk(), needs to be tested

--- Inode.java
+++ Inode.java
@@ -1,3 +1,9 @@
+/**
+Worked on by: Renee
+	Purpose: Each inode represents one file, creates each inode and reads/writes it to the disk
+
+	Satus: complete but not testes
+ */
 public class Inode {
 	public final static int iNodeSize = 32;  // fixed to 32 bytes
     public final static int directSize = 11; // # direct pointers
@@ -59,6 +65,45 @@ public class Inode {
 		// you implement
 		//check corresponding inode on disk
 		//if another thread updated it: read it from the disk and write the contents back
-		//
+		
+		int blkNumber = 1 + iNumber / 16;          // inodes start from block#1
+		byte[] data = new byte[Disk.blockSize]; 
+		SysLib.rawread( blkNumber, data );         // get the inode block
+		int offset = ( iNumber % 16 ) * iNodeSize; // locate the inode top
+
+		if((flag != 0) && (flag != 3)){
+			//this inode (file) has been used by this or other threads
+			//retrive this inode's data from the disk @ iNumber
+			length = SysLib.bytes2int( data, offset ); // retrieve all data members
+			offset += 4;                               // from data
+			count = SysLib.bytes2short( data, offset );
+			offset += 2;
+			flag = SysLib.bytes2short( data, offset );
+			offset += 2;
+			for ( int i = 0; i < directSize; i++ ) {
+				direct[i] = SysLib.bytes2short( data, offset );
+				offset += 2;
+			}
+			indirect = SysLib.bytes2short( data, offset );
+			offset += 2;
+		}
+
+		//write inode back to disk @ iNumber
+		//put all inode attributes in data array
+		int offset = ( iNumber % 16 ) * iNodeSize; // reset offset to top of inode
+		SysLib.int2bytes( length, data, offset ); // add all data member to the data[]
+		offset += 4;                               
+		SysLib.short2bytes( count, data, offset );
+		offset += 2;
+		SysLib.short2bytes( flag, data, offset );
+		offset += 2;
+		for ( int i = 0; i < directSize; i++ ) {
+			SysLib.short2bytes( direct[i], data, offset );
+			offset += 2;
+		}
+		SysLib.short2bytes( indirect, data, offset );
+		offset += 2;
+		SysLib.rawwrite(blkNumber, data);
+
 	}
 }
